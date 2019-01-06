44e41eeadf49d75c42ee5816eaf55560e13509a6
mesuttuncayandex <46408066+mesuttuncayandex@users.noreply.github.com>
2019-01-06 17:13:54 -0500

    Update Calculator.java

--- src/main/java/assignment1/Calculator.java
+++ src/main/java/assignment1/Calculator.java
@@ -2,5 +2,20 @@ package assignment1;
 
 public class Calculator {
 
+public static int add(int v1, int v2){
+        int total = v1+v2;
+        return v1+v2;
+    }
 
+    public static int subtract(int v1, int v2){
+        return v1-v2;
+    }
+
+    public static int multiply(int v1, int v2){
+        return v1*v2;
+    }
+
+    public static int div(int v1, int v2){
+        return v1/v2;
+    }
 }
